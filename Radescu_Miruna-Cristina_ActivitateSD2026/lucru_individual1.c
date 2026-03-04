fea1ee2fec14e9e3ef7da0403cbc479065f1d051
Miruna1506 <mirunaradescu7@gmail.com>
2026-03-04 12:43:47 +0200

    Adăugare funcții pentru copiere, afișare și dezalocare vector de structuri Carte.

--- Radescu_Miruna-Cristina_ActivitateSD2026/lucru_individual1.c
+++ Radescu_Miruna-Cristina_ActivitateSD2026/lucru_individual1.c
@@ -19,6 +19,11 @@ struct Carte initializare(int id,int nrPagini, char * titlu,float pret,char *gen
 	strcpy(c.gen, gen);
 	return c;
 }
+struct Carte deep_copy(struct Carte c)
+{
+	struct Carte carte = initializare(c.id, c.nrPagini, c.titlu, c.pret, c.gen); 
+	return carte;
+}
 void afisare(struct Carte c) {
 	printf("\nId:%d\n", c.id);
 	printf("nrPagini:%d\n", c.nrPagini);
@@ -38,13 +43,43 @@ void dezalocare(struct Carte* c) {
 		free(c->gen);
 		c->gen = NULL;
 	}
-
 }
-void afiseaza_dupa_gen(struct Carte* c, int n, const char* gen_cautat) {
+void dezalocare_vector(struct Carte** vector, int *nrElemente)
+{
+	for (int i = 0; i < *nrElemente; i++) {
+		dezalocare(&vector[i]);
+	}
+	free(vector);
+}
+void afisare_vector(struct Carte* vector, int nrElemente)
+{
+	printf("\n-- Librarie --\n");
+	//afisarea elementelor din vector apeland functia afisare
+	for (int i = 0;i < nrElemente;i++)
+	{
+		afisare(vector[i]);
+	}
+}
+struct Carte* copiazaPrimeleNElemente(struct Carte* vector, int nrElemente, int nrElementeCopiate) {
+	//copiem intr-un vector nou pe care il vom returna primele nrElementeCopiate
+	struct Carte *vectorNou=NULL;
+	vectorNou = (struct Carte*)malloc(sizeof(struct Carte) * nrElementeCopiate);
+	if (nrElemente < nrElementeCopiate)
+	{
+		nrElementeCopiate = nrElemente;
+	}
+		printf("\n-- Dupa copiere --\n");
+		for (int i = 0;i < nrElementeCopiate;i++)
+		{
+			vectorNou[i] = deep_copy(vector[i]); //trebuie facut deep copy, deoarece daca voi dezaloca vectorul original=> dangling pointer
+		}
+	return vectorNou;
+}
+void afiseaza_dupa_gen(struct Carte* vector, int n, const char* gen_cautat) {
 	printf("\n--- Carti din genul %s ---", gen_cautat);
 	for (int i = 0; i < n; i++) {
-		if (strcmp(c[i].gen, gen_cautat) == 0) {
-			afisare(c[i]);
+		if (strcmp(vector[i].gen, gen_cautat) == 0) {
+			afisare(vector[i]);
 		}
 	}
 }
@@ -63,11 +98,15 @@ int main() {
 	librarie[0] = initializare(1, 100, "Harry Potter", 50, "fantasy");
 	librarie[1] = initializare(2, 300, "Dune", 80, "SF");
 	librarie[2] = initializare(3, 150, "Hobbitul", 45, "fantasy");
+
+	afisare_vector(librarie, nrCarti);
 	afiseaza_dupa_gen(librarie,nrCarti,"fantasy");
 	calculeaza_valoare_stoc(librarie, nrCarti);
 
-	for (int i = 0; i < nrCarti; i++) {
-		dezalocare(&librarie[i]);
-	}
-	free(librarie);
+	struct Carte* rezultat;
+	int nrCopiate = 2;
+	rezultat = copiazaPrimeleNElemente(librarie,nrCarti,nrCopiate);
+	afisare_vector(rezultat,nrCopiate);
+
+	dezalocare_vector(&librarie,&nrCarti);
 }
